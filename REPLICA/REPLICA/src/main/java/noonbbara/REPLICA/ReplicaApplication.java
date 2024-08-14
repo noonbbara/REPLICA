1085e1c5db6842e05005682abf1cf58c2ae40895
noonbbara <ysys1226@naver.com>
2024-08-15 01:45:00 +0900

    [INITIAL] 프로젝트 생성

---
+++ REPLICA/REPLICA/src/main/java/noonbbara/REPLICA/ReplicaApplication.java
@@ -0,0 +1,13 @@
+package noonbbara.REPLICA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplicaApplication.class, args);
+	}
+
+}
---
+++ REPLICA/REPLICA/src/test/java/noonbbara/REPLICA/ReplicaApplicationTests.java
@@ -0,0 +1,13 @@
+package noonbbara.REPLICA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
